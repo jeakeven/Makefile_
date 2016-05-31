d91bfe9dc3e12658e61d6c779353e73da4274cac
小麻酱 <jiangtengfei007@163.com>
2016-05-31 17:28:08 +0800

    Add source code

---
+++ EasyMakefile_/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "HelloWorld.h"
+
+using namespace std;
+
+void hello()
+{
+	cout << "Hello World !" << endl;	
+}
---
+++ EasyMakefile_/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "HelloWorld.h"
+
+using namespace std;
+
+int main()
+{
+	hello();
+
+	cout << "This is a test program!" << endl;
+	
+	return 0;
+}
